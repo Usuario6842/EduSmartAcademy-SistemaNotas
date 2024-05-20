f15ed09e51237376eeb768b2952103392e59e386
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2024-05-20 13:05:26 -0400

    db: Agregar tabla Tutor con columnas(id, id_est, nombre, apellido, parentesco, telefono, direccion)

--- database.sql
+++ database.sql
@@ -17,6 +17,19 @@ CREATE TABLE Estudiante(
 	FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
+CREATE TABLE Tutor(
+	id_tutor SERIAL PRIMARY KEY,
+	ci INT Unique,
+	id_estudiante INT,
+	nombre VARCHAR(80) NOT NULL,
+	apellido VARCHAR(100) NOT NULL,
+	parentesco VARCHAR(40) NOT NULL,
+	telefono INT NOT NULL,
+	direccion VARCHAR(150) NOT NULL,
+	FOREIGN KEY (id_estudiante) REFERENCES Estudiante(id_estudiante)
+);
+
+
 
 
 
