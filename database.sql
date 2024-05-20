abe6ee2094894e1072fd0dbabaad17b70271fd2f
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2024-05-20 13:02:45 -0400

    db: Agregar tabla Estudiante con columnas(id, id_usuario, nombre, apellido, genero, f_nacimiento, telefono)

--- database.sql
+++ database.sql
@@ -6,4 +6,17 @@ CREATE TABLE Usuario(
 	pass VARCHAR(20) NOT NULL
 );
 
+CREATE TABLE Estudiante(
+	id_estudiante SERIAL PRIMARY KEY,
+	id_usuario INT,
+	nombre VARCHAR(80) NOT NULL,
+	apellido VARCHAR(100) NOT NULL,
+	genero VARCHAR(30) NOT NULL,
+	f_nacimiento DATE NOT NULL,
+	telefono INT,
+	FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
+);
+
+
+
 
