b9fa40527da4cbc92bbd4b86d27855bc1c2c6cef
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2024-06-03 00:16:27 -0400

    init: Inicializar proyecto Spring Boot

---
+++ Backend/EduSmartAcademyAPI/src/test/java/com/academy/edusmart/EduSmartAcademyAPI/EduSmartAcademyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.edusmart.EduSmartAcademyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSmartAcademyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
