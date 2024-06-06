10d7b32d5a410eb9dd3bb297ed4d9e659f0883fd
callaelmer <callaelmer299@gmail.com>
2024-06-06 00:41:23 -0400

    model: definir la entidad y dto's para Rol

---
+++ Backend/EduSmartAcademyAPI/src/main/java/com/academy/edusmart/EduSmartAcademyAPI/model/dto/response/RolResponse.java
@@ -0,0 +1,34 @@
+package com.academy.edusmart.EduSmartAcademyAPI.model.dto.response;
+
+public class RolResponse {
+    private long id;
+    private String nombre;
+    private String descripcion;
+
+    public RolResponse() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
---
+++ Backend/EduSmartAcademyAPI/src/main/java/com/academy/edusmart/EduSmartAcademyAPI/model/entity/Rol.java
@@ -0,0 +1,50 @@
+package com.academy.edusmart.EduSmartAcademyAPI.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Rol")
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rol")
+    private long id;
+    @Column
+    private String nombre;
+    @Column
+    private String descripcion;
+
+    public Rol() {
+    }
+
+    public Rol(long id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
+
