a47fa6d092f10403eb936f386d01a6a26d69b7fc
callaelmer <callaelmer299@gmail.com>
2024-06-06 00:46:45 -0400

    repo: Agregar metodos de acceso de datos para Rol

---
+++ Backend/EduSmartAcademyAPI/src/main/java/com/academy/edusmart/EduSmartAcademyAPI/repository/RolRepository.java
@@ -0,0 +1,8 @@
+package com.academy.edusmart.EduSmartAcademyAPI.repository;
+
+import com.academy.edusmart.EduSmartAcademyAPI.model.entity.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Long> {
+
+}
