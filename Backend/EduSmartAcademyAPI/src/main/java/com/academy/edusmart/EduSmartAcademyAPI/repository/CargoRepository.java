de6f278af32bf0251c9a4d5067c9280d924306fc
JoaquinCoaritiAliaga LevyDevCode <164289720+LevyDevCode@users.noreply.github.com.>
2024-06-06 00:50:20 -0400

    repo: Agregar metodos de acceso de datos para Cargo

---
+++ Backend/EduSmartAcademyAPI/src/main/java/com/academy/edusmart/EduSmartAcademyAPI/repository/CargoRepository.java
@@ -0,0 +1,8 @@
+package com.academy.edusmart.EduSmartAcademyAPI.repository;
+
+import com.academy.edusmart.EduSmartAcademyAPI.model.entity.Cargo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CargoRepository extends JpaRepository<Cargo, Long> {
+}
+
