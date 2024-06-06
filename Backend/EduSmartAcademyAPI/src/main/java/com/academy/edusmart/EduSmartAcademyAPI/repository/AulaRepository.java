b4f11eca85c6f1724e627a5ffbe76b98fde45f69
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2024-06-05 22:57:14 -0400

    repo: Agregar metodos de acceso de datos para Aula

---
+++ Backend/EduSmartAcademyAPI/src/main/java/com/academy/edusmart/EduSmartAcademyAPI/repository/AulaRepository.java
@@ -0,0 +1,7 @@
+package com.academy.edusmart.EduSmartAcademyAPI.repository;
+
+import com.academy.edusmart.EduSmartAcademyAPI.model.entity.Aula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AulaRepository extends JpaRepository<Aula, Long> {
+}
