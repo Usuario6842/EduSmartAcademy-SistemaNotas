eb6f72cbd71740767ac6ea4182953fe28e078631
JoaquinCoaritiAliaga LevyDevCode <164289720+LevyDevCode@users.noreply.github.com.>
2024-06-13 00:34:18 -0400

    model: Definir entidad y dto's para Estudiante

---
+++ Backend/EduSmartAcademyAPI/src/main/java/com/academy/edusmart/EduSmartAcademyAPI/model/entity/Estudiante.java
@@ -0,0 +1,98 @@
+package com.academy.edusmart.EduSmartAcademyAPI.model.entity;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table (name = "Estudiante")
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_estudiante")
+    private Long id;
+    @Column
+    private String nombre;
+    @Column
+    private String apellido;
+    @Column
+    private String genero;
+    @Column
+    private LocalDate f_nacimiento;
+    @Column
+    private Integer telefono;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(Long id, String nombre, String apellido, String genero, LocalDate f_nacimiento, Integer telefono, Usuario usuario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.f_nacimiento = f_nacimiento;
+        this.telefono = telefono;
+        this.usuario = usuario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public LocalDate getF_nacimiento() {
+        return f_nacimiento;
+    }
+
+    public void setF_nacimiento(LocalDate f_nacimiento) {
+        this.f_nacimiento = f_nacimiento;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
